68c4c6f836918f1ec5c37d5d144abfa1c5cfa8cc
igregorioneto <igregoriodoneto@outlook.com>
2021-08-11 14:26:26 -0300

    UN - Ajustando tabela do dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServiceService } from 'src/app/shared/service.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  unidades: any[] = []
+
+  constructor(
+    private unidadeService: ServiceService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    let unidade: any[]
+
+    this.unidadeService.listarUnidades()
+    .subscribe(u => {
+      unidade = u.sort((a:any, b:any) => b.id - a.id)
+      this.unidades = unidade.slice(0,5)
+
+    })
+  }
+
+  informacaoUnidade(u: any) {
+    this.router.navigate([`/unidades/${u.id}/informacao`])
   }
 
 }
